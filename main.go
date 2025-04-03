5027e5f58fbcd6d5aedbce44e210653f154308e4
e-pallad <eric.pallad@devfluent.de>
2025-04-03 13:49:45 +0200

    first concepts

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world")
+	var myName string = "John Doe"
+	const mySecondName string = "Jane Doe"
+
+	myThridName := "Bob Doe"
+	fmt.Println(myName)
+	fmt.Println(mySecondName)
+	fmt.Println(myThridName)
+}
